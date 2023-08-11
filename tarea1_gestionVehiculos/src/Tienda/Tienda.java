9d530ddb2b94b9f8297b253266b524c705d7f0bb
soma322 <vampari58@gmail.com>
2023-08-11 13:58:19 -0600

    cambios en interaccion

--- tarea1_gestionVehiculos/src/Tienda/Tienda.java
+++ tarea1_gestionVehiculos/src/Tienda/Tienda.java
@@ -1,12 +1,13 @@
 package Tienda;
 
 import java.util.ArrayList;
-
+import java.util.Scanner;
 public abstract class Tienda {
 	protected String nombre;
 	protected String direccion;
 	protected int telefono;
 	protected ArrayList<Productos> inventario;
+	Scanner scan;
 	
 	public Tienda(String nombre, String direccion,int telefono) {
 		this.nombre = nombre;
@@ -20,6 +21,7 @@ public abstract class Tienda {
 	public abstract void realizarPago(double monto, String tipoPago);
 	public abstract void verInventario();
 	public abstract void agregarProducto(String nombre,	int precio,	int cantidad);
+	
 	
 	
 	
--- tarea1_gestionVehiculos/src/Tienda/interraccion.java
+++ tarea1_gestionVehiculos/src/Tienda/interraccion.java
@@ -30,7 +30,7 @@ public class interraccion {
 		}
 	}while(flagTerminar);
 	if(tipoTienda != 0) {
-		datosTienda();
+		
 		tienda(tipoTienda);
 	}
 	
@@ -41,17 +41,74 @@ public class interraccion {
 	}
 	
 	public static void tienda(int tipoTienda) {
-		
+		String nombre = "";
+		String direccion = "";
+		String telefono = "";
+		Tienda tienda;
 		switch(tipoTienda) {
 		case 1:
-			//Tienda tienda = new TiendaEfectivo(String); 
+			nombre = getDatos("nombre");
+			direccion = getDatos("Direccion");
+			telefono = getDatos("telefono");
+			if(esNumerico(telefono)) {
+				tienda = new TiendaEfectivo(nombre,direccion,Integer.parseInt(telefono)); 
+			}else {
+				return;
+			}
+			
 			break;
 		case 2:
+			nombre = getDatos("nombre");
+			direccion = getDatos("Direccion");
+			telefono = getDatos("telefono");
+			if(esNumerico(telefono)) {
+				tienda = new TiendaTarjeta(nombre,direccion,Integer.parseInt(telefono)); 
+			}else {
+				return;
+			}
 			break;
 		case 3:
+			nombre = getDatos("nombre");
+			direccion = getDatos("Direccion");
+			telefono = getDatos("telefono");
+			if(esNumerico(telefono)) {
+				tienda = new TiendaMixta(nombre,direccion,Integer.parseInt(telefono)); 
+			}else {
+				return;
+			}
 			break;
 			
 		}
+		boolean flagTerminar = true;
+		Scanner scan = new Scanner(System.in);
+		do {
+			System.out.println("Bienvenido a la gestion de Tiendas");
+			System.out.println("Que operacion deseas realizar?");
+			System.out.println("1.- Agregar Producto");
+			System.out.println("2.- Realizar Cobro");
+			System.out.println("3.- Realizar Pago");
+			System.out.println("4.- Ver inventario");
+			System.out.println("5.- Terminar");
+			int n = scan.nextInt();
+			switch(n) {
+			case 1:
+				tienda.agregarProducto()
+				break;
+			case 2:
+				
+				break;
+			case 3:
+				break;
+			case 4:
+				flagTerminar = false;
+				break;
+			default:
+				System.out.println("Favor de seleccionar una opcion");
+				break;
+			
+			}
+		}while(flagTerminar);
+		
 	}
 	
 	public static int tipoTienda() {
@@ -81,6 +138,24 @@ public class interraccion {
 		}while(flagTerminar);
 		return 0;
 		
+	}
+	public static String getDatos(String dato)
+	{
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Favor de escribir el: "+dato);
+		String n = scan.nextLine();
+		return n;
+	}
+	public static boolean esNumerico(String cad) {
+		if (cad == null) {
+			return false;
+		}
+		try {
+			int numero = Integer.parseInt(cad); 
+		}catch (NumberFormatException x) {
+			return false;
+		}
+		return true;
 	}
 
 }
