9a4f5212d9082cbf2bcdbd20bcbebf7db721293f
soma322 <vampari58@gmail.com>
2023-10-09 01:45:09 -0600

    enum

--- blackjack/src/blackjack/EnumCartas.java
+++ blackjack/src/blackjack/EnumCartas.java
@@ -23,7 +23,19 @@ public class EnumCartas {
 	}
 
 	public enum blackjack{
-		AS("AS", 1);
+		AS("AS", 1),
+		DOS("DOS", 2),
+	    TRES("TRES", 3),
+	    CUATRO("CUATRO", 4),
+	    CINCO("CINCO", 5),
+	    SEIS("SEIS", 6),
+	    SIETE("SIETE", 7),
+	    OCHO("OCHO", 8),
+	    NUEVE("NUEVE", 9),
+	    DIEZ("DIEZ", 10),
+	    JOTA("JOTA", 10),
+	    REINA("REINA", 10),
+	    REY("REY", 10);
 	   
 	
 		private final String nombre;
